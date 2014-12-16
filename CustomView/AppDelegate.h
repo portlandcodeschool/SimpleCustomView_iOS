6596e0c412b7b2eaf23a00d349866f67ae3f3c3b
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2014-12-15 20:30:47 -0800

    Initial Commit

---
+++ CustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomView
+//
+//  Created by Erick Bennett on 12/15/14.
+//  Copyright (c) 2014 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomView
+//
+//  Created by Erick Bennett on 12/15/14.
+//  Copyright (c) 2014 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
